5ba2620e2bda9ff3d4bc414d27f7eaab0fc54992
Olaandrew8 <olaandrew8@gmail.com>
2023-08-10 11:35:45 -0700

    write function

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - c program that print a line to a standard error
- * Return: 1
+ * main - using the write function to print
+ *
+ * Return: always (0)
+ *
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\r", stdout);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
