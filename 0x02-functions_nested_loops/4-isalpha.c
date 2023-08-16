d78c835b951452d9da0a562f352f8b6c58c0955f
Olaandrew8 <olaandrew8@gmail.com>
2023-08-16 14:09:14 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _isalpha - check for alphabetic character
+ * @c: collect the character
+ *
+ * Return: 1 if c is low or cap, otherwise 0
+ */
+int _isalpha(int c)
+{
+	char lower, upper;
+	int letter = 0;
+	for (lower = 'a'; lower <= 'z'; lower++)
+	{
+		for (lower = 'A'; upper <= 'Z'; upper++)
+		{
+			if (lower == c || upper == c)
+			{
+				letter = 1;
+			}
+		}
+	}
+	return (letter);
+}
