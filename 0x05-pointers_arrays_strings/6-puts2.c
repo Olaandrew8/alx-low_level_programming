78c1fbf5457207fbd16c63f96cc44fed4f49c298
Olaandrew8 <olaandrew8@gmail.com>
2023-08-21 08:07:36 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,28 +1,20 @@
 #include "main.h"
 /**
  * puts2 - fun prints one chac out of two starting with the first one
- * @str: Irout
- * Return: print
+ * @str: used string reference
+ * Return: 0
  */
 void puts2(char *str)
 {
-	int longi = 0;
-	int t = 0;
-	char *y = str;
-	int o;
+	int a = 0;
 
-	while (*y != '\0')
+	while (str[a] != '\0')
 	{
-		y++;
-		longi++;
-	}
-	t = longi - 1;
-	for (o = 0 ; o <= t ; o++)
-	{
-		if (o % 2 == 0)
+		if (a % 2 == 0)
 		{
-			_putchar(str[0]);
+			putchar(str[a]);
 		}
+		a++;
 	}
 	_putchar('\n');
 }
