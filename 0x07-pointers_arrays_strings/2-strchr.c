45036229e10117383606b68849d1f96ce7c45f45
Olaandrew8 <olaandrew8@gmail.com>
2023-08-28 05:48:48 -0700

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_strchr - func locates a character in a string
+ * @s: string to search
+ * @c: char to find
+ *
+ * Return: a pointer to the first instance of the chac
+ * c in the string s, or NULL if the chac is not found
+ */
+char *_strchr(char *s, char c)
+{
+		int z;
+
+		while (1)
+		{
+			z = *s++;
+			if (z == c)
+			{
+				return (s - 1);
+			}
+			if (z == 0)
+			{
+				return (NULL);
+			}
+		}
+}
